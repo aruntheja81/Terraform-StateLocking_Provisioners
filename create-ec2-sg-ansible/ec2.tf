b8d9d7d540250de96029060c673aa97053b0781a
iwayq Technology <iwayqtech@gmail.com>
2019-10-21 12:22:01 +0000

    ec2.tf edited online with Bitbucket

--- create-ec2-sg-ansible/ec2.tf
+++ create-ec2-sg-ansible/ec2.tf
@@ -1,8 +1,8 @@
 #provider block
 
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
+  #access_key = "${var.access_key}"
+  #secret_key = "${var.secret_key}"
   region     = "${var.region}"
 }
 
